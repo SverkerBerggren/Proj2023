b580847ddbf734cffd689470368f09a1b95e5f9e
SverkerBerggren <svartaspjutet@gmail.com>
2023-11-06 20:43:33 +0100

    Initial commit

---
+++ Source/Proj2023/Proj2023.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Proj2023.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Proj2023, "Proj2023" );
